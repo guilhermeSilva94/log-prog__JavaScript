98a48634d74c7cbdbf8ce4c2b685f694934f0e41
Guilherme Silva <guidss3394@gmail.com>
2022-09-03 00:00:55 -0300

    Ex002 - Extra

---
+++ ex002/calcula_consumo.js
@@ -0,0 +1,11 @@
+let tanque = 40;
+let caminhoComGasolina = 480;
+let caminhoComAlcool = 300;
+
+let consumoDeGasolina = caminhoComGasolina/tanque;
+
+document.write('O consumo de Gasolina foi de ' + consumoDeGasolina + ' Km/l');
+
+let consumoDeAlcool = caminhoComAlcool/tanque;
+
+document.write('<br>O consumo de Álcool foi de ' + consumoDeAlcool + ' Km/l');
