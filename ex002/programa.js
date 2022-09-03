04e4de926ca50df0b4596084834146bcf9e8faae
Guilherme Silva <guidss3394@gmail.com>
2022-09-02 23:49:37 -0300

    Ex002

---
+++ ex002/programa.js
@@ -0,0 +1,19 @@
+let ano = 2016;
+
+document.write("Flávio tem  " + (ano - 1977) + " anos" + "<br>");
+document.write("Joaquim tem " + (ano - 1996) + " anos" + "<br>");
+
+ano = 2017;
+document.write("Barney tem " + (ano - 1976) + " anos" + "<br>");
+
+let idadeFlavio = 39;
+let idadeJoaquim = 20;
+let idadeBarney = 41;
+
+let media = (idadeFlavio + idadeJoaquim + idadeBarney)/3;
+document.write("A média das idades é " + Math.round(media) + "<br>");
+
+let nome = "Flávio";
+
+document.write("A idade de " + nome + " é " + idadeFlavio);
+
